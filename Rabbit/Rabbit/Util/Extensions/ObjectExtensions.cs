5cc66c90bec94f3e898b38fab640f17e21802be1
yingyejun <yingyejun202@vip.qq.com>
2015-06-19 13:03:15 +0800

    修改参数命名

--- Rabbit/Rabbit/Util/Extensions/ObjectExtensions.cs
+++ Rabbit/Rabbit/Util/Extensions/ObjectExtensions.cs
@@ -12,13 +12,13 @@ namespace Rabbit.Util.Extensions
         /// </summary>
         /// <typeparam name="T">对象类型。</typeparam>
         /// <param name="instance">对象实例。</param>
-        /// <param name="name">参数名称。</param>
+        /// <param name="paramName">参数名称。</param>
         /// <returns>对象实例。</returns>
         /// <exception cref="ArgumentNullException"><paramref name="instance"/> 为null。</exception>
-        public static T NotNull<T>(this T instance, string name) where T : class
+        public static T NotNull<T>(this T instance, string paramName) where T : class
         {
             if (instance == null)
-                throw new ArgumentNullException(name.NotEmpty("name"));
+                throw new ArgumentNullException(paramName.NotEmpty("paramName"));
             return instance;
         }
 
@@ -26,13 +26,13 @@ namespace Rabbit.Util.Extensions
         /// 不允许空字符串。
         /// </summary>
         /// <param name="str">字符串。</param>
-        /// <param name="name">参数名称。</param>
+        /// <param name="paramName">参数名称。</param>
         /// <returns>字符串。</returns>
         /// <exception cref="ArgumentNullException"><paramref name="str"/> 为空。</exception>
-        public static string NotEmpty(this string str, string name)
+        public static string NotEmpty(this string str, string paramName)
         {
             if (string.IsNullOrEmpty(str))
-                throw new ArgumentNullException(name.NotEmpty("name"));
+                throw new ArgumentNullException(paramName.NotEmpty("paramName"));
             return str;
         }
 
@@ -40,13 +40,13 @@ namespace Rabbit.Util.Extensions
         /// 不允许空和只包含空格的字符串。
         /// </summary>
         /// <param name="str">字符串。</param>
-        /// <param name="name">参数名称。</param>
+        /// <param name="paramName">参数名称。</param>
         /// <returns>字符串。</returns>
         /// <exception cref="ArgumentNullException"><paramref name="str"/> 为空或者全为空格。</exception>
-        public static string NotEmptyOrWhiteSpace(this string str, string name)
+        public static string NotEmptyOrWhiteSpace(this string str, string paramName)
         {
             if (string.IsNullOrWhiteSpace(str))
-                throw new ArgumentNullException(name.NotEmpty("name"));
+                throw new ArgumentNullException(paramName.NotEmpty("paramName"));
             return str;
         }
     }
